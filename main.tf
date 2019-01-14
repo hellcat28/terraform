07a3859d8d83a9659c819dca6ccd3c895d0f0080
Luat Dang <dltrongluat@gmail.com>
2019-01-14 09:59:36 +0700

    add a .gitignore and tf file

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "aws"
 {
-  access_key = "AKIAJFXEHDSUOSQ5Z2VA"
-  secret_key = "VUBGdqzSq25hVAB4Z1rRBdk0PeDhPjpyTxLOiSjX"
   region = "us-east-1"
 }
 resource "aws_instance" "example"
@@ -9,6 +7,6 @@ resource "aws_instance" "example"
   ami = "ami-40d28157"
   instance_type = "t2.micro"
   tags {
-    Name = "terraform-example"
+    Name = "terraform-exampless"
   }
  }
