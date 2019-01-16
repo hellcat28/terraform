80975723f488f0ad4eb0226ad39e78e51b5ebd48
Luat Dang <dltrongluat@gmail.com>
2019-01-16 08:12:17 +0000

    wtf

---
+++ example.tf
@@ -0,0 +1,16 @@
+provider "aws"
+{
+  region     = "us-east-1"
+}
+
+resource "aws_eip" "ip" {
+  instance = "${aws_instance.example.id}"
+}
+resource "aws_instance" "example" {
+  ami           = "ami-b374d5a5"
+  instance_type = "t2.micro"
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
+}
